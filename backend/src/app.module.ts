38c8c0cdb8788e406653eb6fc1cae1ebad01f1fe
Emerson Moreira <eemr3@yahoo.com.br>
2023-12-09 11:43:54 -0300

    feat: add config swagger in main.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaModule } from './confing/database/prisma/prisma.module';
 import { ProfessionalModule } from './app/professional/professional.module';
 import { TypeProfessionalModule } from './app/type-professional/type-professional.module';
+import { PrismaModule } from './confing/database/prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule, ProfessionalModule, TypeProfessionalModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: '*' });
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  app.setGlobalPrefix('/api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('API Professional and Professional Type')
+    .setDescription(
+      'Esta aplicação tem como objetivo poder cadastrar profissional e seu tipo de profissão. ',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/doc', app, document);
+
+  await app.listen(3001);
 }
 bootstrap();

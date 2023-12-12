1a01e6dcd3ceb18865b9d5ec21f14973c565339b
Emerson Moreira <eemr3@yahoo.com.br>
2023-12-11 21:58:11 -0300

    feat: create componente (page) UpdateTypeProfessional

---
+++ frontend/src/pages/TypeProfessional/Update.tsx
@@ -0,0 +1,9 @@
+import UpdateTypeProfessionalForm from '../../components/Forms/TypeProfessional/Update';
+
+export default function UpdateTypeProfessional() {
+  return (
+    <div>
+      <UpdateTypeProfessionalForm />
+    </div>
+  );
+}
